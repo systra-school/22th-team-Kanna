a50eef27cb7ada731ca77c9d983f72220cdeb7be
ryusei_ota <or3apple@gmail.com>
2024-02-29 13:31:59 +0900

    障害No.6 ページ：月別シフト確認（#4, MTH001） 概要：表示年月日の選択の空白を選択時にエラーになる 内容：空白の選択しを除去 更新者：太田

--- src/main/java/action/mth/TsukibetsuShiftKakuninSearchAction.java
+++ src/main/java/action/mth/TsukibetsuShiftKakuninSearchAction.java
@@ -86,7 +86,8 @@ public class TsukibetsuShiftKakuninSearchAction extends TsukibetsuShiftNyuuryoku
         // セレクトボックスの取得
         ComboListUtilLogic comboListUtils = new ComboListUtilLogic();
         Map<String, String> shiftCmbMap = comboListUtils.getComboShift(true);
-        Map<String, String> yearMonthCmbMap = comboListUtils.getComboYearMonth(CommonUtils.getFisicalDay(CommonConstant.yearMonthNoSl), 3, ComboListUtilLogic.KBN_YEARMONTH_NEXT, true);
+        Map<String, String> yearMonthCmbMap = comboListUtils.getComboYearMonth(CommonUtils.getFisicalDay(CommonConstant.yearMonthNoSl), 3, ComboListUtilLogic.KBN_YEARMONTH_NEXT, false);
+        //年月日の表示の空白をfalseに修正（2024/02/29　太田）
 
         if (CheckUtils.isEmpty(tsukibetsuShiftDtoMap)) {
             // データなし
